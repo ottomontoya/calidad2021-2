41bd081df010a2d790b61a64c6385f0f4f0ab576
Otto Montoya <montoyaotto@icloud.com>
2021-09-23 17:38:54 -0600

    Mod desde JetBrains

--- src/main/java/com/anahuac/calidad/app/App.java
+++ src/main/java/com/anahuac/calidad/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "En brew confío y esto está editado" );
+        System.out.println( "En brew confío y esto está editado");
+        System.out.println( "Blablabla pt2");
     }
 }
