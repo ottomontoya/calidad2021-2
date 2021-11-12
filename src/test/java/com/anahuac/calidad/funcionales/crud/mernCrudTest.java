fd21ea4a138c4df66f08578f3e924fdfcf099f77
Otto Montoya <montoyaotto@icloud.com>
2021-11-11 22:04:54 -0600

    added merCrudTest with the pkg

---
+++ src/test/java/com/anahuac/calidad/funcionales/crud/mernCrudTest.java
@@ -0,0 +1,110 @@
+package com.anahuac.calidad.funcionales.crud;
+
+import java.util.Random;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class mernCrudTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    JavascriptExecutor js;
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void testCrud() throws Exception {
+        // en vez de crear o deletear un nuevo mail, lo que hago es crear un numero random, esto hace que no sea tan
+        // inconveniente la creacion de esto.
+        int numRandom = new Random().nextInt();
+
+        driver.get("https://mern-crud.herokuapp.com/");
+        driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("name")).clear();
+        driver.findElement(By.name("name")).sendKeys("brandon otell");
+        driver.findElement(By.name("email")).clear();
+
+        // Aqui agrego el numRandom
+        driver.findElement(By.name("email")).sendKeys("dashy" + String.valueOf(numRandom) +"@gmail.com");
+
+        driver.findElement(By.name("age")).clear();
+        driver.findElement(By.name("age")).sendKeys("21");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+
+        wait(5);
+
+        String tag = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/div")).getText();
+        assertThat("Nice one!", is(tag));
+
+        //driver.findElement(By.xpath("//i")).click();
+    }
+
+    public void wait(int sec) {
+        try {
+            Thread.currentThread().sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
